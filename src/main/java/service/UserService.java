d61c154df2895641f81e159689d40fb818f1ee62
RadhaKulkarni <64362689+RadhaKulkarni@users.noreply.github.com>
2020-07-26 17:13:47 +0530

    Update UserService.java

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,41 +1,12 @@
 package service;
 
-import java.util.List;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import model.User;
-import repository.UserRepository;
 
-@Service
-public class UserService {
-
-	@Autowired
-    private UserRepository userRepository;
+public interface UserService {
+	  
+	 public User findUserByUsername(String username);
+	 
+	 //public void saveUser(User user);no need cz there is no registration
 	
-	
-	public UserService() {
-    }
 
-    public User createUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public Optional<User> getUser(Long id) {
-        return userRepository.findById(id);
-    }
-
-    public void updateUser(User user) {
-    	userRepository.save(user);
-    }
-
-    public void deleteUser(Long id) {
-    	userRepository.deleteById(id);
-    }
-    
-    public List<User> getAllUsers()
-    {
-    	return userRepository.findAll();
-    }
 }
